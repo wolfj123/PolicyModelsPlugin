f427a03215707b905bab04b5c125eff8b259e08f
Unknown <wolfj@post.bgu.ac.il>
2019-12-24 10:37:50 +0200

    made test script wait for user input (useful if fails)

--- lsp_samples/lsp_samples/scripts/e2e.sh
+++ lsp_samples/lsp_samples/scripts/e2e.sh
@@ -4,3 +4,5 @@ export CODE_TESTS_PATH="$(pwd)/client/out/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
 
 node "$(pwd)/client/node_modules/vscode/bin/test"
+
+read -rsp $'Press enter to continue...\n'
