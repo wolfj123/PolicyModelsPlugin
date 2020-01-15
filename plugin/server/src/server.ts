84744b0925d875b9761798b58e4406dc50f14d0b
Unknown <wolfj@post.bgu.ac.il>
2020-01-15 10:27:29 +0200

    added textmate to server project
    auto rebuilds using electron-rebuild

--- plugin/server/src/server.ts
+++ plugin/server/src/server.ts
@@ -218,76 +218,14 @@ connection.onCompletionResolve(
 );
 
 
+ import {runme} from './textmate_playing_around';
+
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
 	//validateTextDocument(change.document);
-	validateBrackets(change.document);
+	//runme();  //jonathan: I added this just to play around with textmate and see the print
 });
-
-
-
-
-//import * as vsctm from "vscode-textmate";
-const vsctm = require('vscode-textmate');
-const fs = require('fs');
-
-
-/**
- * Utility to read a file as a promise
- */
-function readFile(path : any) : Promise<any> {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, (error : any, data : any) => error ? reject(error) : resolve(data));
-    })
-}
-
-// Create a registry that can create a grammar from a scope name.
-const registry = new vsctm.Registry({
-    loadGrammar: (scopeName : string) => {
-        // if (scopeName === 'string.quoted.double.policyspace') {
-		// 	let cwd = __dirname;
-		// 	return readFile(cwd + '\\..\\..\\syntaxes\\policyspace.tmLanguage.json').then(data => vsctm.parseRawGrammar(data.toString()))
-			
-		// }
-		if (scopeName === 'source.ts') {
-			let cwd = __dirname;
-			return readFile(cwd + '\\..\\..\\syntaxes\\typescript.tmLanguage').then(data => vsctm.parseRawGrammar(data.toString()))
-			
-		}
-        console.log(`Unknown scope name: ${scopeName}`);
-        return null;
-    }
-});
-
-
-async function validateBrackets(textDocument: TextDocument) : Promise<void> {
-	// Load the JavaScript grammar and any other grammars included by it async.
-	registry.loadGrammar('source.ts').then( (grammar : any) => {	
-		const text = [
-			`[idk]`
-		];
-		let ruleStack = vsctm.INITIAL;
-		for (let i = 0; i < text.length; i++) {
-			const line = text[i];
-			if(grammar == null) {break;};
-			const lineTokens = grammar.tokenizeLine(line, ruleStack);
-			console.log(`\nTokenizing line: ${line}`);
-			for (let j = 0; j < lineTokens.tokens.length; j++) {
-				const token = lineTokens.tokens[j];
-				console.log(` - token from ${token.startIndex} to ${token.endIndex} ` +
-				`(${line.substring(token.startIndex, token.endIndex)}) ` +
-				`with scopes ${token.scopes.join(', ')}`
-				);
-			}
-			ruleStack = lineTokens.ruleStack;
-		}
-	});
-
-	validateTextDocument(textDocument);
-}
-
-
 
 
 /*
@@ -320,7 +258,7 @@ connection.listen();
 
 function runModel(param : string[]) : string{
 	console.log("server is running the model")
-	let cwd = __dirname + "\\..\\..\\";
-	child_process.execSync(`start cmd.exe /K java -jar "${cwd}\\cli\\DataTagsLib.jar"`);
+	let cwd = __dirname + "/../../";
+	child_process.execSync(`start cmd.exe /K java -jar "${cwd}/cli/DataTagsLib.jar"`);
 	return "execute ends";
 }
