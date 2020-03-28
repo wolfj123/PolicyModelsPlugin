50508e87a42ed9dd748ef24b9c0cac81cb2c317e
Unknown <wolfj@post.bgu.ac.il>
2020-03-28 19:03:41 +0300

    valueinference support

--- plugin/client/src/color/colors.ts
+++ plugin/client/src/color/colors.ts
@@ -182,6 +182,62 @@ export function colorPolicySpace(root: Parser.Tree, visibleRanges: {start: numbe
 	])
 }
 
+export function colorValueInference(root: Parser.Tree, visibleRanges: {start: number, end: number}[]) {
+	const functions: Range[] = []
+	const variables: Range[] = []
+
+	let visitedChildren = false
+	let cursor = root.walk()
+	let parents = [cursor.nodeType]
+	while (true) {
+		// Advance cursor
+		if (visitedChildren) {
+			if (cursor.gotoNextSibling()) {
+				visitedChildren = false
+			} else if (cursor.gotoParent()) {
+				parents.pop()
+				visitedChildren = true
+				continue
+			} else {
+				break
+			}
+		} else {
+			const parent = cursor.nodeType
+			if (cursor.gotoFirstChild()) {
+				parents.push(parent)
+				visitedChildren = false
+			} else {
+				visitedChildren = true
+				continue
+			}
+		}
+		// Skip nodes that are not visible
+		if (!visible(cursor, visibleRanges)) {
+			visitedChildren = true
+			continue
+		}
+		// Color tokens
+		const parent = parents[parents.length - 1]
+		const grandparent = parents[parents.length - 2]
+		switch (cursor.nodeType) {
+			case 'identifier_simple':
+				//if (parent == 'slot' || (parent == 'identifier_with_desc' && grandparent == 'slot')) {
+					functions.push({start: cursor.startPosition, end: cursor.endPosition})
+				//}
+				//else {
+				//	variables.push({start: cursor.startPosition, end: cursor.endPosition})
+				//}
+			break
+		}
+	}
+
+	return new Map([
+		['entity.name.function', functions],
+		['variable', variables],
+	])
+}
+
+
 function isVisible(x: Parser.SyntaxNode, visibleRanges: {start: number, end: number}[]) {
 	for (const {start, end} of visibleRanges) {
 		const overlap = x.startPosition.row <= end+1 && start-1 <= x.endPosition.row
