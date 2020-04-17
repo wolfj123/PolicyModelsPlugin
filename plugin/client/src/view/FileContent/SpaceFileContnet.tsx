ffa210b6552d712839a71b48587dce05928a3bbf
OFIR <ofirb@wix.com>
2020-04-17 21:11:27 +0300

    add localization - stage 2

---
+++ plugin/client/src/view/FileContent/SpaceFileContnet.tsx
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import { SetStateAction, Dispatch } from 'react';
+
+interface Props {
+  content: string;
+  onFileChange(content: string): void;
+}
+
+interface spaceDataSection {
+  slot: string;
+  name: string;
+  shortNote: string;
+  longNote: string;
+}
+
+const SpaceFileContnet: React.FunctionComponent<Props> = props => {
+  const { content, onFileChange } = props;
+  const [spaceData, setFileData]: [spaceDataSection[], Dispatch<SetStateAction<spaceDataSection[]>>] = React.useState(
+    parseContentToSpaceData(content)
+  );
+
+  function parseContentToSpaceData(content): spaceDataSection[] {
+    const slotsBlocks = cleanArrayFromSpaces(content.split('#'));
+    return slotsBlocks.map(block => {
+      const singleSlot: spaceDataSection = {
+        slot: '',
+        name: '',
+        shortNote: '',
+        longNote: '',
+      };
+      const reg = /[^\r\n]+/g;
+      const linesWithoutSpaces = block.match(reg);
+      const originalStr = linesWithoutSpaces.join('\n');
+      let separatedBlocks = originalStr.split('\n---\n');
+
+      while (separatedBlocks.length > 1) {
+        singleSlot['longNote'] = separatedBlocks.pop() + singleSlot['longNote'];
+      }
+      separatedBlocks = separatedBlocks.pop().split('\n');
+      if (separatedBlocks.length > 3 && separatedBlocks[3] !== '---') {
+        throw new Error('Cannot parse File');
+      }
+
+      singleSlot['slot'] = separatedBlocks.length > 0 ? separatedBlocks.shift() : '';
+      singleSlot['name'] = separatedBlocks.length > 0 ? separatedBlocks.shift() : '';
+      singleSlot['shortNote'] = separatedBlocks.length > 0 ? separatedBlocks.shift() : '';
+      return singleSlot;
+    });
+  }
+
+  React.useEffect(() => onFileChange(parseSpaceDataToContent()), [spaceData]);
+
+  function parseSpaceDataToContent() {
+    let newContent = '';
+    spaceData.forEach(data => {
+      const { slot, name, shortNote, longNote } = data;
+      newContent += `# ${slot}\n`;
+      name && (newContent += `${name}\n`);
+      shortNote && (newContent += `${shortNote}\n`);
+      longNote && (newContent += `---\n`);
+      longNote && (newContent += `${longNote}\n`);
+    });
+    return newContent;
+  }
+
+  const onChangeInput = (event: any, i, field) => {
+    const newValue = event.target.value;
+    const newSpaceData = [...spaceData];
+    newSpaceData[i][field] = newValue;
+    setFileData(newSpaceData);
+  };
+
+  const createSlotComponent = (slotData, i) => {
+    const { slot, name, shortNote, longNote } = slotData;
+    return (
+      <div className="slot">
+        <h1>{slot}</h1>
+        <form>
+          <label>
+            Name
+            <br />
+            <input type="text" value={name} onChange={e => onChangeInput(e, i, 'name')} />
+          </label>
+        </form>
+        <form>
+          <label>
+            Short Note
+            <br />
+            <input type="text" value={shortNote} onChange={e => onChangeInput(e, i, 'shortNote')} />
+          </label>
+        </form>
+        <form>
+          <label>
+            Long Note
+            <p>
+              <textarea style={{ width: '100%', height: '150px' }} value={longNote} onChange={e => onChangeInput(e, i, 'longNote')} />
+            </p>
+          </label>
+        </form>
+      </div>
+    );
+  };
+  return <>{spaceData.map(createSlotComponent)}</>;
+};
+
+function cleanArrayFromSpaces(array) {
+  return array.filter(element => element !== '');
+}
+
+export default SpaceFileContnet;
--- plugin/client/src/view/LocalizationContainer/LocalizationContainer.tsx
+++ plugin/client/src/view/LocalizationContainer/LocalizationContainer.tsx
@@ -1,48 +1,94 @@
 import * as React from 'react';
-import Text from '../Text/Text';
 import FileContent from '../FileContent/FileContent';
 import CompareToPanel from '../CompareToPanel/CompareToPanel';
+import SideBarMenu from '../SideBarMenu/SideBarMenu';
+import { LanguageData,ItemMenuData } from '../Types/model';
 import './LocalizationContainer.css';
 
+
 interface Props {
-  languageFilesData: any;
+  languageFilesData: LanguageData[];
   onSave(path: string, content: string): void;
 }
 
-const LocalizationContainer: React.FunctionComponent<Props> = ({ languageFilesData, onSave }) => {
-  const [selectedLanguage, setSelectedLanguage] = React.useState(languageFilesData[0]);
-  const [selectedFile, setSelectedFile] = React.useState(languageFilesData[0].files[0]);
 
-  const handleFileChange = (path,content)=>{
-    onSave(path,content);
+const LocalizationContainer: React.FunctionComponent<Props> = ({ languageFilesData, onSave }) => {
+  const [selectedLanguageId, setSelectedLanguageId] = React.useState(languageFilesData[0].language);
+  const [selectedFileId, setSelectedFileId] = React.useState(languageFilesData[0].files[0].id);
+  const [previewFileId, setPreviewFileId] = React.useState(languageFilesData[0].files[0].id);
+  const [previewLanguageName, setPreviewLanguageName] = React.useState(languageFilesData[0].language);
+
+  const selectedLanguage = languageFilesData.find(data => data.language=== selectedLanguageId);
+  const selectedFile = selectedLanguage.files.find(file => file.id===selectedFileId);
+
+
+  const getFileFromId = id =>{
+    const allFiles = languageFilesData.reduce((filesAcc,languages) => filesAcc.concat(languages.files),[]);
+    return allFiles.find(file => file.id===id)
   }
 
-  const languagesMenu = languageFilesData.map(data => {
-    const isSelected = selectedLanguage.language === data.language;
-    return (
-      <Text onClick={() => {setSelectedLanguage(data); setSelectedFile(data.files[0]);}} isSelected={isSelected}>
-        {data.language}
-      </Text>
-    );
+  const getFileByNameAndLanguageName = (fileName,languageName) =>{
+    const langaugeData = languageFilesData.find(language => language.language===languageName);
+    const langaugeFiles = langaugeData.files;
+    const file = langaugeFiles.find(file => file.name===fileName);
+    return file;
+  }
+
+  const setPreviewFileFromSelectedLanguage = language => {
+    const currentFileName = selectedFile.name;
+    const newPreviewFile = getFileByNameAndLanguageName(currentFileName,language);
+    setPreviewFileId(newPreviewFile.id);
+    setPreviewLanguageName(language);
+  }
+
+  const handleFileChange = (path, content) => {
+    onSave(path, content);
+  };
+
+  const languagesMenuData: ItemMenuData[] = languageFilesData.map((data) => {
+    return {
+      isSelected: selectedLanguage.language === data.language,
+      onClick: () => {
+        setSelectedLanguageId(data.language);
+        setSelectedFileId(data.files[0].id);
+      },
+      text: data.language,
+    };
   });
 
 
-  const filesList = selectedLanguage.files.map(file => {
-    const isSelected = selectedFile.id === file.id;
-    return (
-      <Text onClick={() => setSelectedFile(file)} isSelected={isSelected}>
-        {file.name}
-      </Text>
-    );
+
+   const filesMenuData: ItemMenuData[] = selectedLanguage.files.map((file) => {
+    return {
+      isSelected: selectedFile.id === file.id,
+      onClick: () => setSelectedFileId(file.id),
+      text: file.name,
+    };
   });
 
-  const languagesNames = languageFilesData.map(languageData => languageData.language);
+  React.useEffect(()=>setPreviewFileFromSelectedLanguage(previewLanguageName),[selectedFileId]);
+
+  const previewFile = getFileFromId(previewFileId);
+  const languagesNames = languageFilesData.map((languageData) => languageData.language);
   return (
-    <div className={'container'}>
-      <div className="panel">{languagesMenu}</div>
-      <div className="panel">{filesList}</div>
-      <div className="panel"><FileContent key={selectedFile.id} fileData={selectedFile} onFileChange={handleFileChange}/></div>
-      <div className="panel"><CompareToPanel key={selectedFile.id} languages={languagesNames} contentData={selectedFile} onSelectLanguage={language => console.log(`${language} is selected`)}/></div>
+    <div className={'App'}>
+      <div className={'container'}>
+      <div className="panel" style={{flex: '0 0 115px'}}>
+        <SideBarMenu filesMenuData={filesMenuData} languagesMenuData={languagesMenuData} />
+      </div>
+      <div className="panel" style={{flex: '1 1 0'}}>
+        <FileContent key={selectedFile.id} fileData={selectedFile} onFileChange={handleFileChange} />
+      </div>
+      <div className="panel" style={{flex: '1 1 0'}}>
+        <CompareToPanel
+          key={selectedFile.id}
+          languages={languagesNames}
+          previewFile={previewFile}
+          previewLanguageName={previewLanguageName}
+          onSelectLanguage={language => setPreviewFileFromSelectedLanguage(language)}
+        />
+      </div>
+      </div>
     </div>
   );
 };
