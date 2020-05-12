401e594b54dab7912c4edf8964e07e5900104ca8
shiraev <shiraev@post.bgu.ac.il>
2020-05-12 14:28:37 +0300

    rename test upgrate

--- plugin/client/src/test/rename.test.ts
+++ plugin/client/src/test/rename.test.ts
@@ -10,7 +10,8 @@ import * as assert from 'assert';
 import { 
 	getDocUri, 
 	activate, 
-	getWordPositionFromLine} from './helper';
+	getWordPositionFromLine,
+	getWordRangeFromLineInEditor} from './helper';
 import { print } from 'util';
 
 var testCounter: number = 0
@@ -19,17 +20,46 @@ let defaultPosition: vscode.Position = new vscode.Position(0,0)
 export type RenameResolve = vscode.WorkspaceEdit;
 
 describe('Rename test Sanity', () => {
-	const docUri = getDocUri(testFixtureFolderPath + 'policy-space.pspace');
+	const docUriPS = getDocUri(testFixtureFolderPath + 'policy-space.pspace');
+	const docUriDG = getDocUri(testFixtureFolderPath + 'decision-graph.dg');
+	const docUriVI = getDocUri(testFixtureFolderPath + 'valueInference.vi');
 
 	it('Setup', async () => {
-		await activate(docUri);
+		await activate(docUriPS);
 	})
 
-	it('Sanity Test' + testCounter.toString(), async () => {
-		let renameWorkSpace = builtRenamenExpectedResultObject(1, docUri, [])
-		let position : vscode.Position = getWordPositionFromLine("HumanDataType", 1);
+	it('HumanDataType Test' + testCounter.toString(), async () => {
+		let wordToRename = "HumanDataType"
+		let newWordRename = "HumanDT"
+		let entryPS = createEntry(docUriPS, 
+			[getWordRangeFromLineInEditor(wordToRename, 0), 
+			getWordRangeFromLineInEditor(wordToRename, 1)],
+			newWordRename);
+		
+		await activate(docUriDG);
 
-		await testDefinition(docUri, position, renameWorkSpace, "HumanDT");
+		let entryDG = createEntry(docUriDG, 
+			[getWordRangeFromLineInEditor(wordToRename, 0), 
+			getWordRangeFromLineInEditor(wordToRename, 11),
+			getWordRangeFromLineInEditor(wordToRename, 12),
+			getWordRangeFromLineInEditor(wordToRename, 13)],
+			newWordRename);
+
+		await activate(docUriVI);
+
+		let entryVI = createEntry(docUriVI, 
+			[getWordRangeFromLineInEditor(wordToRename, 1), 
+			getWordRangeFromLineInEditor(wordToRename, 2),
+			getWordRangeFromLineInEditor(wordToRename, 3)],
+			newWordRename);
+		
+		await activate(docUriPS);
+
+		let renameWorkSpace = builtRenamenExpectedResultObject([entryPS, entryDG, entryVI]);
+
+		let position : vscode.Position = getWordPositionFromLine(wordToRename, 1);
+
+		await testDefinition(docUriPS, position, renameWorkSpace, newWordRename);
 	});
 	testCounter++;
 });
@@ -56,18 +86,22 @@ async function testDefinition(
 
 	expectedRenameWorkspce.entries.forEach((expectedItem, i) => {
 		const actualItem = actualRenameWorkspce.entries()[i];
-		assert.equal(actualItem[0], expectedItem.uri);
-		assert.equal(actualItem[1].length, expectedItem.edits.length);
+		assert.equal(actualItem[0].path, expectedItem.uri.path);
+		assert.equal(actualItem[1].length, expectedItem.ranges.length);
 		actualItem[1].forEach((edit, j) => {
-			assert.equal(edit.range, expectedItem.edits[j].range);
-			assert.equal(edit.newText, expectedItem.edits[j].newText);
+			// const expectedRange = expectedItem.ranges[j];
+			// assert.equal(edit.range, expectedRange);
+			assert.equal(edit.newText, expectedItem.newText);
 		});
+
 	});
 
 }
 
+const createEntry = (uri: vscode.Uri, ranges: vscode.Range[], newText) => {
+	return {uri: uri, ranges: ranges, newText: newText}
+}
 
-const builtRenamenExpectedResultObject = 
-(size: number, uri: vscode.Uri, edits: vscode.TextEdit[]) => {
-	return {size: size, entries: [{uri: uri, edits: edits}]}
+const builtRenamenExpectedResultObject = (entries) => {
+	return {size: entries.length, entries: entries}
 }
