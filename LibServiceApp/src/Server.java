8b8aa72fbbde6ef73f110132c3bf9b7331751780
OFIR <ofirb@wix.com>
2020-05-16 15:45:51 +0300

    Phase 1 complete

---
+++ LibServiceApp/src/Server.java
@@ -0,0 +1,33 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+
+    public static void main(String[] args) {
+        try {
+            activeServer();
+            System.out.print("ready");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void activeServer() throws IOException {
+        ExecutorService executor =  Executors.newSingleThreadExecutor();
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 5001), 0);
+        server.createContext("/load", new LoadModelHandler());
+        server.createContext("/loc/new", new CreateNewLocalizationhandler());
+        server.createContext("/loc/update", new UpdateLocalizationHandler());
+        server.setExecutor(executor);
+        server.start();
+    }
+}
+
+
--- client/src/services/PolicyModelLibApi.ts
+++ client/src/services/PolicyModelLibApi.ts
@@ -1,61 +1,97 @@
 const axios = require('axios');
+const SUCCESS = true;
 const PORT = 5001;
 const BASE_URL = `http://localhost:${PORT}`;
-axios.defaults.timeout = 10000;
-// var spawn = require('child_process').spawn, child;
 import * as path from 'path';
-
-
+import { ChildProcess } from 'child_process';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  timeout: 1500,
 });
 
 export class PolicyModelLibApi {
   _rootPath: string;
+  child: ChildProcess;
+  _printToScreenCallback: any;
 
-  constructor(rootPath) {
+  constructor(rootPath, printToScreenCallback) {
     this._rootPath = rootPath;
+    this._printToScreenCallback = printToScreenCallback;
+    this.child;
   }
 
-  async _buildEnviroment() {
+  async _buildEnvironment() {
     let isSucceed: boolean = true;
     isSucceed = isSucceed && await this._startServer();
-
     isSucceed = isSucceed && await this._loadModel();
     return isSucceed;
   }
 
-  _startServer() {
-    const JavaServerJar: string = path.join(__dirname, "/../../../cli/JarCommunicatingTool.jar");
-    var child = require('child_process').spawn(
-      'java', ['-jar', `${JavaServerJar}`, 'argument to pass in']
+  async _startServer(): Promise<boolean> {
+    const JavaServerJar: string = path.join(__dirname, "/../../../cli/LibServiceApp.jar");
+    this.child = require('child_process').spawn(
+      'java', ['-jar', `${JavaServerJar}`, null]
     );
-    child.stdout.on('data', function(data) {
-      console.log(data.toString());
-      child.kill('SIGINT');
 
-      child.stderr.on("data", function (data) {
-        console.log(data.toString());
-    });
-  });
-    // child = spawn(`java -jar "${JavaServerJar}"`,{async:true,silent:true});
-      // function (error, stdout, stderr) {
-      //   console.log('stdout: ' + stdout);
-      //   console.log('stderr: ' + stderr);
-      //   if (error !== null) {
-      //     console.log('exec error: ' + error);
-      //   }
-      // });
-    return true;
+    const serverIsReady = async (): Promise<boolean> => {
+      return new Promise<boolean>((resolve) => {
+        this.child.stdout.on('data', data => {
+          const message: string = data.toString();
+          message === 'ready' ?
+            resolve(true) :
+            this._printToScreen(message);
+        });
+
+        this.child.stderr.on("data", data => {
+          this._printToScreen(data.toString());
+          this._terminateProcess();
+          resolve(false);
+
+        });
+      });
+    };
+
+    return await serverIsReady();
   }
 
-  _loadModel = () => axiosInstance.get(`/load?${this._rootPath}`).then(res => res == 'true');
+  async _loadModel() {
+    return await axiosInstance.get(`/load?path=${this._rootPath}`).then((res: any) => {
+      return res.data === SUCCESS;
+    }).catch(this._handleConnectionRejection);;
+  }
+  _printToScreen(message: string): void {
+    this._printToScreenCallback(message);
+  }
+
+  _handleConnectionRejection(err: any): void {
+    this._printToScreen(err.message);
+  }
+
+  _terminateProcess(): void {
+    this.child.kill('SIGINT');
+  }
+
+  async _createNewLocalization(name: string): Promise<boolean> {
+    return await axiosInstance.get(`/loc/new?name=${name}`).then((res: any) => res.data === SUCCESS).catch(this._handleConnectionRejection);
+  }
+
+  async _requestsWrapper(requestCallback) {
+    const buildSucceeded = await this._buildEnvironment();
+    let requestAnswer = false;
+    if (buildSucceeded) {
+      requestAnswer = await requestCallback();
+    }
+    this._terminateProcess();
+    return requestAnswer;
+  }
+
+
+  setPrintToScreenCallback(callback) {
+    this._printToScreenCallback = callback;
+  }
 
   async createNewLocalization(name: string): Promise<boolean> {
-    return await this._loadModel().then((isSucceed: boolean) =>
-      isSucceed ?
-        axiosInstance.get(`/loc/new?${name}`).then((res: string) => res == 'true') :
-        false);
+    return await this._requestsWrapper(() => this.createNewLocalization(name));
   }
 }
